f23129e6f62070f22521ca74bab908d6708208f8
Giannis Daras <daras.giannhs@gmail.com>
2024-11-09 12:34:55 -0500

    Install requirements with the package

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("requirements.txt", "r") as f:
+    requirements = [line.strip() for line in f.readlines()]
+
 setuptools.setup(
     name="ambient_utils",
     version="0.0.1",
@@ -13,9 +16,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/giannisdaras/ambient_utils",
     packages=setuptools.find_packages(),
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)", 
         "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Intended Audience :: Science/Research",
